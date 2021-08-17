acb86a5662a3ca584700b753c061340556bd56ea
dederobert <adinquer@yahoo.com>
2021-08-17 17:36:35 +0200

    Initial commit

---
+++ src/main/java/fr/orezia/mc/tri/api/OreziaTriApi.java
@@ -0,0 +1,21 @@
+package fr.orezia.mc.tri.api;
+
+import fr.orezia.mc.oreziacore.api.annotation.PublicApi;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Entry point for plugin OreziaTri
+ * @since 3.1
+ */
+@PublicApi
+public interface OreziaTriApi {
+
+  /**
+   * Get the plugin name.
+   *
+   * @return The plugin name.
+   */
+  @PublicApi
+  @NotNull String pluginName();
+
+}
