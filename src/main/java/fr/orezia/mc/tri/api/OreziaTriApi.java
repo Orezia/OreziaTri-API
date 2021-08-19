1b5d66beafb3d7837556cc59f31b62debf9e09e8
dederobert <adinquer@yahoo.com>
2021-08-19 21:55:40 +0200

    feat: Create OreziaCore-api (#2)

--- src/main/java/fr/orezia/mc/tri/api/OreziaTriApi.java
+++ src/main/java/fr/orezia/mc/tri/api/OreziaTriApi.java
@@ -1,11 +1,13 @@
 package fr.orezia.mc.tri.api;
 
+import fr.orezia.mc.core.api.annotation.PublicApi;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Entry point for plugin OreziaTri
  * @since 3.1
  */
+@PublicApi
 public interface OreziaTriApi {
 
   /**
@@ -13,6 +15,7 @@ public interface OreziaTriApi {
    *
    * @return The plugin name.
    */
+  @PublicApi
   @NotNull String pluginName();
 
 }
