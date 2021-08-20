2dafd5d3e0d92ced21ef04e8eae5ca70aa1c9477
dederobert <adinquer@yahoo.com>
2021-08-20 21:12:01 +0200

    build: Fix dependencies (#3)

--- src/main/java/fr/orezia/mc/tri/api/OreziaTriApi.java
+++ src/main/java/fr/orezia/mc/tri/api/OreziaTriApi.java
@@ -1,21 +1,13 @@
 package fr.orezia.mc.tri.api;
 
+import fr.orezia.mc.core.api.OreziaPluginApi;
 import fr.orezia.mc.core.api.annotation.PublicApi;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Entry point for plugin OreziaTri
  * @since 3.1
  */
 @PublicApi
-public interface OreziaTriApi {
-
-  /**
-   * Get the plugin name.
-   *
-   * @return The plugin name.
-   */
-  @PublicApi
-  @NotNull String pluginName();
+public interface OreziaTriApi extends OreziaPluginApi {
 
 }
